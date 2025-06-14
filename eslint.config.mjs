45f586e186c7bc29b902802c342286ceef1b48a7
ROG <luan.roger.2003@gmail.com>
2025-06-14 19:56:28 -0300

    chore: install Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,27 @@
 import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const prettierIgnorePath = path.resolve(__dirname, ".prettierignore");
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
+  includeIgnoreFile(prettierIgnorePath),
+  eslintConfigPrettier,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
