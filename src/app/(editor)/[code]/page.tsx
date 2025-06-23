1eaaa2b24a057f4e455d9e4fc037d2c02c18f6c2
ROG <luan.roger.2003@gmail.com>
2025-06-23 19:12:34 -0300

    fix: check the code before create page

--- src/app/(editor)/[code]/page.tsx
+++ src/app/(editor)/[code]/page.tsx
@@ -1,5 +1,7 @@
+import { validateSlug } from "@/utils/slug";
 import { ensureCreated } from "../../actions/notes";
 import Editor from "@/components/editor";
+import { notFound } from "next/navigation";
 
 export default async function Page({
   params,
@@ -7,6 +9,11 @@ export default async function Page({
   params: Promise<{ code: string }>;
 }) {
   const { code } = await params;
+
+  const isValidCode = validateSlug(code);
+  if (!isValidCode) {
+    notFound();
+  }
 
   const note = await ensureCreated(code);
   const body = note?.body;
