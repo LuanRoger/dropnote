205643cd76c5e4a1bf08ff818f614d2e13370af8
ROG <luan.roger.2003@gmail.com>
2025-12-16 14:47:28 -0300

    refactor: move DB realted code to databse package

--- packages/database/schemas/index.ts
+++
@@ -1,8 +0,0 @@
-/** biome-ignore-all lint/performance/noNamespaceImport: Need to import all to Drizzle recoganize */
-import * as auth from "./auth";
-import * as page from "./pages";
-
-export const schemas = {
-  ...auth,
-  ...page,
-};
--- packages/database/schemas/pages.ts
+++
@@ -1,6 +0,0 @@
-import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
-
-export const pages = pgTable("page", {
-  id: serial("id"),
-  name: varchar("name", { length: 256 }),
-});
