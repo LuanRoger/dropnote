a12b8d7e4cf496823bce3ed2e330070843f59c01
ROG <luan.roger.2003@gmail.com>
2025-12-16 14:28:09 -0300

    chore: update core env keys

--- packages/next-config/keys.ts
+++ packages/next-config/keys.ts
@@ -9,14 +9,12 @@ export const keys = () =>
     },
     client: {
       NEXT_PUBLIC_APP_URL: z.url(),
-      NEXT_PUBLIC_WEB_URL: z.url(),
       NEXT_PUBLIC_PROJECT_PRODUCTION_URL: z.url().optional(),
     },
     runtimeEnv: {
       ANALYZE: process.env.ANALYZE,
       NEXT_RUNTIME: process.env.NEXT_RUNTIME,
       NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-      NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,
       NEXT_PUBLIC_PROJECT_PRODUCTION_URL: process.env.PROJECT_PRODUCTION_URL,
     },
   });
