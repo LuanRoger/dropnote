c2f184aaea5e67960732bb0ebd64582513c5193c
ROG <luan.roger.2003@gmail.com>
2025-12-30 09:36:45 -0300

    feat: update general metadata

--- packages/seo/metadata.ts
+++ packages/seo/metadata.ts
@@ -1,29 +1,29 @@
 import merge from "lodash.merge";
 import type { Metadata } from "next";
+import {
+  applicationName,
+  productionUrl,
+  protocol,
+  publisher,
+} from "./constants";
 
 type MetadataGenerator = Omit<Metadata, "description" | "title"> & {
-  title: string;
+  title?: string;
   description: string;
-  image?: string;
 };
 
-const applicationName = "next-forge";
 const author: Metadata["authors"] = {
-  name: "Vercel",
-  url: "https://vercel.com/",
+  name: "Luan Roger",
+  url: "https://www.luanroger.dev/",
 };
-const publisher = "Vercel";
-const twitterHandle = "@vercel";
-const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
-const productionUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL;
 
 export const createMetadata = ({
   title,
   description,
-  image,
   ...properties
 }: MetadataGenerator): Metadata => {
-  const parsedTitle = `${title} | ${applicationName}`;
+  const parsedTitle = title ? `${title} | ${applicationName}` : applicationName;
+
   const defaultMetadata: Metadata = {
     title: parsedTitle,
     description,
@@ -49,24 +49,8 @@ export const createMetadata = ({
       locale: "en_US",
     },
     publisher,
-    twitter: {
-      card: "summary_large_image",
-      creator: twitterHandle,
-    },
   };
 
   const metadata: Metadata = merge(defaultMetadata, properties);
-
-  if (image && metadata.openGraph) {
-    metadata.openGraph.images = [
-      {
-        url: image,
-        width: 1200,
-        height: 630,
-        alt: title,
-      },
-    ];
-  }
-
   return metadata;
 };
