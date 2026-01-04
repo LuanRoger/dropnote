4d1fe33c3ef2e916bc03acf1322856450ac922f5
ROG <luan.roger.2003@gmail.com>
2026-01-04 13:35:40 -0300

    feat: pass the editor is env vars

--- apps/app/env.ts
+++ apps/app/env.ts
@@ -1,10 +1,11 @@
 import { keys as database } from "@repo/database/keys";
+import { keys as editor } from "@repo/editor/keys";
 import { keys as core } from "@repo/next-config/keys";
 import { keys as product } from "@repo/product/keys";
 import { createEnv } from "@t3-oss/env-nextjs";
 
 export const env = createEnv({
-  extends: [product(), core(), database()],
+  extends: [product(), core(), database(), editor()],
   server: {},
   client: {},
   runtimeEnv: {},
