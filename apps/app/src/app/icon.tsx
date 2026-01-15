08190e3a9021daafec47103a983bf20f849c0600
ROG <luan.roger.2003@gmail.com>
2026-01-15 08:53:03 -0300

    feat: change favicon to dynamic

---
+++ apps/app/src/app/icon.tsx
@@ -0,0 +1,39 @@
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+import { ImageResponse } from "next/og";
+
+export const contentType = "image/png";
+
+export default async function Icon() {
+  const geistFont = await readFile(
+    join(process.cwd(), "public", "Geist-Bold.ttf"),
+  );
+
+  return new ImageResponse(
+    <div
+      style={{
+        fontSize: 24,
+        background: "black",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "Geist",
+        color: "white",
+      }}
+    >
+      d.
+    </div>,
+    {
+      width: 32,
+      height: 32,
+      fonts: [
+        {
+          name: "Geist",
+          data: geistFont,
+        },
+      ],
+    },
+  );
+}
