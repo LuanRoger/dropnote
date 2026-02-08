f33295044a2e8b3b41c5378071af7f6481335aed
ROG <luan.roger.2003@gmail.com>
2026-02-08 16:26:15 -0300

    refactor: type Notes schema

--- packages/database/schemas/notes.ts
+++ packages/database/schemas/notes.ts
@@ -1,12 +1,21 @@
 import mongoose from "mongoose";
 
+export interface NoteModel {
+  code: string;
+  // biome-ignore lint/suspicious/noExplicitAny: The body of the note can be any, since we don't know the structure of the note
+  body: any;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
 export const notesSchema = new mongoose.Schema(
   {
     code: { type: String, unique: true, required: true },
     body: Object,
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 export const Notes =
-  mongoose.models.Notes || mongoose.model("Notes", notesSchema);
+  (mongoose.models.Notes as mongoose.Model<NoteModel>) ||
+  mongoose.model<NoteModel>("Notes", notesSchema);
