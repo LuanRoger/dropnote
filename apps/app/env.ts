1bf2a46d81e780bcfea47938b77be82b96fc4284
ROG <luan.roger.2003@gmail.com>
2026-02-21 14:22:50 -0300

    fix: update url and file name of exports

--- apps/app/env.ts
+++ apps/app/env.ts
@@ -3,17 +3,10 @@ import { keys as editor } from "@repo/editor/keys";
 import { keys as core } from "@repo/next-config/keys";
 import { keys as product } from "@repo/product/keys";
 import { createEnv } from "@t3-oss/env-nextjs";
-import z from "zod";
 
 export const env = createEnv({
   extends: [product(), core(), database(), editor()],
-  server: {
-    HOCUSPOCUS_API_URL: z.url(),
-    HOCUSPOCUS_API_KEY: z.string(),
-  },
+  server: {},
   client: {},
-  runtimeEnv: {
-    HOCUSPOCUS_API_URL: process.env.HOCUSPOCUS_API_URL,
-    HOCUSPOCUS_API_KEY: process.env.HOCUSPOCUS_API_KEY,
-  },
+  runtimeEnv: {},
 });
