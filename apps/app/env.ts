bdd17e731ca5cbad4706b997bb39aeebc01f96d7
ROG <luan.roger.2003@gmail.com>
2026-02-22 15:50:34 -0300

    feat: add email package in app

--- apps/app/env.ts
+++ apps/app/env.ts
@@ -1,12 +1,13 @@
 import { keys as database } from "@repo/database/keys";
 import { keys as editor } from "@repo/editor/keys";
+import { keys as email } from "@repo/email/keys";
 import { keys as core } from "@repo/next-config/keys";
 import { keys as product } from "@repo/product/keys";
 import { keys as security } from "@repo/security/keys";
 import { createEnv } from "@t3-oss/env-nextjs";
 
 export const env = createEnv({
-  extends: [product(), core(), database(), editor(), security()],
+  extends: [product(), core(), database(), editor(), security(), email()],
   server: {},
   client: {},
   runtimeEnv: {},
