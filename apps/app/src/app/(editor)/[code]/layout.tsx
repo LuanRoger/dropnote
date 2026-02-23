6c0db090be2f2b04c3e4c3cd26c02957dee8e109
ROG <luan.roger.2003@gmail.com>
2026-02-23 15:24:17 -0300

    fix: made security code input a route in editor parallel route

--- apps/app/src/app/(editor)/[code]/layout.tsx
+++ apps/app/src/app/(editor)/[code]/layout.tsx
@@ -1,34 +1,22 @@
 import { notFound } from "next/navigation";
 import { resolveNoteAccess } from "@/app/actions/notes";
-import {
-  checkIfNoteHasSecurityCode,
-  createSecurityCodeForNote,
-  findSecurityCodeByNoteCode,
-} from "@/app/actions/security-code";
 import { validateSlug } from "@/utils/slug";
 
 type LayoutProps = Readonly<{
   params: Promise<{ code: string }>;
   editor: React.ReactNode;
   password: React.ReactNode;
-  updatepassword: React.ReactNode;
 }>;
 
 export default async function Layout({
   params,
   editor,
   password,
-  updatepassword: updatePassword,
 }: LayoutProps) {
   const { code } = await params;
   const isValidCode = validateSlug(code);
   if (!isValidCode) {
     notFound();
-  }
-
-  const haveSecurityCodePending = await findSecurityCodeByNoteCode(code);
-  if (haveSecurityCodePending) {
-    return updatePassword;
   }
 
   const access = await resolveNoteAccess(code);
